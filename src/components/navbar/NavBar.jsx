bf42632ef9ab32ff1cdde2f52fc7403680450573
Akalchi <alejandrafdez1699@gmail.com>
2025-03-20 13:51:30 +0100

    STYLE - updated NavBar styles and implemented logo

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Navbar.module.css';
+import logoImgDark from '../../assets/artprintLogoDark.png';
 
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
             <div className={styles.logo}>
-                <img src="/logo.png" alt="ArtPrint" />
+                <img src={logoImgDark} alt="ArtPrint" />
             </div>
             <div className={styles.categories}>
                 <ul className={styles.categoryList}>
