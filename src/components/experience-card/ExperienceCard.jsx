1ffa89295097cebeb4d2b7bd3f6cbb08cdc28393
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-03-20 16:55:38 +0100

    FIX- edited viewMoreButton in ExperienceCard to make it cleaner

--- src/components/experience-card/ExperienceCard.jsx
+++ src/components/experience-card/ExperienceCard.jsx
@@ -16,9 +16,7 @@ const ExperienceCard = ({ title, city, format, description, price, image, onClic
         <p className={styles.description}>{description}</p>
         <div className={styles.footer}>
           <span className={styles.price}>{price}€</span>
-          <button className={styles.viewMoreButton} onClick={onClick}>
-            Ver más
-          </button>
+          <button className={styles.viewMoreButton} onClick={onClick}/> 
         </div>
       </div>
     </div>
