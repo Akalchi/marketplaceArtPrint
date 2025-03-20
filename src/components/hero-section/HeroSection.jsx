70013be08acca6705f91a5348fefa870ca0ce126
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-03-20 12:56:04 +0100

    FEAT- added header, h2, styles to HeroSection

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -5,7 +5,8 @@ export default function HeroSection() {
     return (
         <section className={styles.hero}>
             <div className={styles.container}>
-                <h1 className={styles.title}>Un espacio para crear</h1>
+                <h1 className={styles.title}>ArtPrint</h1>
+                <h2 className={styles.subtitle}>Un espacio para crear</h2>
                 <p className={styles.description}>
                     Desconecta, explora y aprende disfrutando
                 </p>
